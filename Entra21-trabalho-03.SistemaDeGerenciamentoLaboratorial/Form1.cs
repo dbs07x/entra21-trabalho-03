c307f3685720aa181e209c774726968102a76583
dbs07x <dbs07x@gmail.com>
2022-07-25 18:07:57 -0300

    Add project files.

---
+++ Entra21-trabalho-03.SistemaDeGerenciamentoLaboratorial/Form1.cs
@@ -0,0 +1,10 @@
+namespace Entra21_trabalho_03.SistemaDeGerenciamentoLaboratorial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
