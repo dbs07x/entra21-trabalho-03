59e06052d6298088bc94a3263158ce6c94600c4d
dbs07x <dbs07x@gmail.com>
2022-07-25 22:10:21 -0300

    Criação do banco de dados e da estrutura de tabelas

---
+++ Entra21-trabalho-03.SistemaDeGerenciamentoLaboratorial/Database/SQLQuery.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE planos(
+	id INTEGER PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(50),
+	abrangencia VARCHAR(12),
+	acomodacao VARCHAR(12),
+	coparticipacao DECIMAL(5,2),
+	preco DECIMAL(6,2));
+
+SELECT * FROM planos
