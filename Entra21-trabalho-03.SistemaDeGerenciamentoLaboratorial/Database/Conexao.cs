17994bbdb5a1b6bb15d3f9062194c15d6a49cdf9
dbs07x <dbs07x@gmail.com>
2022-07-25 22:08:26 -0300

    Criação da classe conexão

---
+++ Entra21-trabalho-03.SistemaDeGerenciamentoLaboratorial/Database/Conexao.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SqlClient;
+
+namespace Entra21_trabalho_03.SistemaDeGerenciamentoLaboratorial.Database
+{
+    internal class Conexao
+    {
+        public SqlConnection Conectar()
+        {
+            SqlConnection conexao = new SqlConnection();
+
+            var connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Douglas\Source\Repos\dbs07x\entra21-trabalho-03\Entra21-trabalho-03.SistemaDeGerenciamentoLaboratorial\Database\Database.mdf;Integrated Security=True";
+
+            conexao.ConnectionString = connectionString;
+
+            conexao.Open();
+
+            return conexao;
+        }
+    }
+}
