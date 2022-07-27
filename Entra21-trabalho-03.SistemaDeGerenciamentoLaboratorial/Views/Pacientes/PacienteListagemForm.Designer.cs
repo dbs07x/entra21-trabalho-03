1367146dc4aa005fae3fb11d2d87a339c3b4c726
Marina De Lima <marinalimasc@gmail.com>
2022-07-27 20:41:10 -0300

    Finalizado PacienteListagemForm

--- Entra21-trabalho-03.SistemaDeGerenciamentoLaboratorial/Views/Pacientes/PacienteListagemForm.Designer.cs
+++ Entra21-trabalho-03.SistemaDeGerenciamentoLaboratorial/Views/Pacientes/PacienteListagemForm.Designer.cs
@@ -29,10 +29,15 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonCadastrar = new System.Windows.Forms.Button();
+            this.buttonEditar = new System.Windows.Forms.Button();
+            this.buttonApagar = new System.Windows.Forms.Button();
             this.ColumnId = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColumnNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Data_Nascimento = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.DataNascimento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnDataNascimento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnCpf = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnTelefone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnEmail = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,13 +47,45 @@
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ColumnId,
             this.ColumnNome,
-            this.Data_Nascimento,
-            this.DataNascimento});
-            this.dataGridView1.Location = new System.Drawing.Point(325, 210);
+            this.ColumnDataNascimento,
+            this.ColumnCpf,
+            this.ColumnTelefone,
+            this.ColumnEmail});
+            this.dataGridView1.Location = new System.Drawing.Point(33, 77);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(643, 351);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // buttonCadastrar
+            // 
+            this.buttonCadastrar.Location = new System.Drawing.Point(716, 56);
+            this.buttonCadastrar.Name = "buttonCadastrar";
+            this.buttonCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.buttonCadastrar.TabIndex = 1;
+            this.buttonCadastrar.Text = "Cadastrar";
+            this.buttonCadastrar.UseVisualStyleBackColor = true;
+            this.buttonCadastrar.Click += new System.EventHandler(this.buttonCadastrar_Click);
+            // 
+            // buttonEditar
+            // 
+            this.buttonEditar.Location = new System.Drawing.Point(716, 85);
+            this.buttonEditar.Name = "buttonEditar";
+            this.buttonEditar.Size = new System.Drawing.Size(75, 23);
+            this.buttonEditar.TabIndex = 2;
+            this.buttonEditar.Text = "Editar";
+            this.buttonEditar.UseVisualStyleBackColor = true;
+            this.buttonEditar.Click += new System.EventHandler(this.buttonEditar_Click);
+            // 
+            // buttonApagar
+            // 
+            this.buttonApagar.Location = new System.Drawing.Point(716, 27);
+            this.buttonApagar.Name = "buttonApagar";
+            this.buttonApagar.Size = new System.Drawing.Size(75, 23);
+            this.buttonApagar.TabIndex = 4;
+            this.buttonApagar.Text = "Apagar";
+            this.buttonApagar.UseVisualStyleBackColor = true;
+            this.buttonApagar.Click += new System.EventHandler(this.buttonApagar_Click);
             // 
             // ColumnId
             // 
@@ -60,21 +97,34 @@
             this.ColumnNome.HeaderText = "Nome";
             this.ColumnNome.Name = "ColumnNome";
             // 
-            // Data_Nascimento
+            // ColumnDataNascimento
             // 
-            this.Data_Nascimento.HeaderText = "ColumnDataNascimento";
-            this.Data_Nascimento.Name = "Data_Nascimento";
+            this.ColumnDataNascimento.HeaderText = "DataNascimento";
+            this.ColumnDataNascimento.Name = "ColumnDataNascimento";
             // 
-            // DataNascimento
+            // ColumnCpf
             // 
-            this.DataNascimento.HeaderText = "ColumnDataNascimento";
-            this.DataNascimento.Name = "DataNascimento";
+            this.ColumnCpf.HeaderText = "CPF";
+            this.ColumnCpf.Name = "ColumnCpf";
+            // 
+            // ColumnTelefone
+            // 
+            this.ColumnTelefone.HeaderText = "Telefone";
+            this.ColumnTelefone.Name = "ColumnTelefone";
+            // 
+            // ColumnEmail
+            // 
+            this.ColumnEmail.HeaderText = "Email";
+            this.ColumnEmail.Name = "ColumnEmail";
             // 
             // PacienteListagemForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonApagar);
+            this.Controls.Add(this.buttonEditar);
+            this.Controls.Add(this.buttonCadastrar);
             this.Controls.Add(this.dataGridView1);
             this.Name = "PacienteListagemForm";
             this.Text = "PacienteListagemForm";
@@ -86,9 +136,14 @@
         #endregion
 
         private DataGridView dataGridView1;
+        private Button buttonCadastrar;
+        private Button buttonEditar;
+        private Button buttonApagar;
         private DataGridViewTextBoxColumn ColumnId;
         private DataGridViewTextBoxColumn ColumnNome;
-        private DataGridViewTextBoxColumn Data_Nascimento;
-        private DataGridViewTextBoxColumn DataNascimento;
+        private DataGridViewTextBoxColumn ColumnDataNascimento;
+        private DataGridViewTextBoxColumn ColumnCpf;
+        private DataGridViewTextBoxColumn ColumnTelefone;
+        private DataGridViewTextBoxColumn ColumnEmail;
     }
 }
