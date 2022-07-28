bb2e659150d64bba08b4ea210a4ff7981a2dd677
dbs07x <dbs07x@gmail.com>
2022-07-28 17:51:06 -0300

    Criado view exame

---
+++ Entra21-trabalho-03.SistemaDeGerenciamentoLaboratorial/Views/Exames/ExameCadastroEdicaoForm.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Entra21_trabalho_03.SistemaDeGerenciamentoLaboratorial.Views.Exames
+{
+    partial class ExameCadastroEdicaoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ExameCadastroEdicaoForm));
+            this.labelNome = new System.Windows.Forms.Label();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.labelPreco = new System.Windows.Forms.Label();
+            this.textBoxPreco = new System.Windows.Forms.TextBox();
+            this.labelInstrucoes = new System.Windows.Forms.Label();
+            this.textBoxInstrucoes = new System.Windows.Forms.TextBox();
+            this.labelMedico = new System.Windows.Forms.Label();
+            this.comboBoxMedico = new System.Windows.Forms.ComboBox();
+            this.buttonSalvar = new System.Windows.Forms.Button();
+            this.buttonCancelar = new System.Windows.Forms.Button();
+            this.labelTitulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelNome
+            // 
+            this.labelNome.AutoSize = true;
+            this.labelNome.Location = new System.Drawing.Point(12, 73);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(40, 15);
+            this.labelNome.TabIndex = 1;
+            this.labelNome.Text = "Nome";
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(12, 91);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(421, 23);
+            this.textBoxNome.TabIndex = 2;
+            // 
+            // labelPreco
+            // 
+            this.labelPreco.AutoSize = true;
+            this.labelPreco.Location = new System.Drawing.Point(15, 117);
+            this.labelPreco.Name = "labelPreco";
+            this.labelPreco.Size = new System.Drawing.Size(37, 15);
+            this.labelPreco.TabIndex = 3;
+            this.labelPreco.Text = "Preço";
+            // 
+            // textBoxPreco
+            // 
+            this.textBoxPreco.Location = new System.Drawing.Point(12, 135);
+            this.textBoxPreco.Name = "textBoxPreco";
+            this.textBoxPreco.Size = new System.Drawing.Size(153, 23);
+            this.textBoxPreco.TabIndex = 4;
+            // 
+            // labelInstrucoes
+            // 
+            this.labelInstrucoes.AutoSize = true;
+            this.labelInstrucoes.Location = new System.Drawing.Point(12, 161);
+            this.labelInstrucoes.Name = "labelInstrucoes";
+            this.labelInstrucoes.Size = new System.Drawing.Size(61, 15);
+            this.labelInstrucoes.TabIndex = 5;
+            this.labelInstrucoes.Text = "Instruções";
+            // 
+            // textBoxInstrucoes
+            // 
+            this.textBoxInstrucoes.Location = new System.Drawing.Point(12, 179);
+            this.textBoxInstrucoes.Name = "textBoxInstrucoes";
+            this.textBoxInstrucoes.Size = new System.Drawing.Size(421, 23);
+            this.textBoxInstrucoes.TabIndex = 6;
+            // 
+            // labelMedico
+            // 
+            this.labelMedico.AutoSize = true;
+            this.labelMedico.Location = new System.Drawing.Point(214, 117);
+            this.labelMedico.Name = "labelMedico";
+            this.labelMedico.Size = new System.Drawing.Size(47, 15);
+            this.labelMedico.TabIndex = 7;
+            this.labelMedico.Text = "Médico";
+            // 
+            // comboBoxMedico
+            // 
+            this.comboBoxMedico.FormattingEnabled = true;
+            this.comboBoxMedico.Location = new System.Drawing.Point(214, 135);
+            this.comboBoxMedico.Name = "comboBoxMedico";
+            this.comboBoxMedico.Size = new System.Drawing.Size(219, 23);
+            this.comboBoxMedico.TabIndex = 8;
+            // 
+            // buttonSalvar
+            // 
+            this.buttonSalvar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonSalvar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(153)))), ((int)(((byte)(93)))));
+            this.buttonSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonSalvar.Font = new System.Drawing.Font("Constantia", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonSalvar.ForeColor = System.Drawing.Color.Black;
+            this.buttonSalvar.Image = ((System.Drawing.Image)(resources.GetObject("buttonSalvar.Image")));
+            this.buttonSalvar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonSalvar.Location = new System.Drawing.Point(11, 227);
+            this.buttonSalvar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonSalvar.Name = "buttonSalvar";
+            this.buttonSalvar.Padding = new System.Windows.Forms.Padding(16, 0, 0, 0);
+            this.buttonSalvar.Size = new System.Drawing.Size(170, 46);
+            this.buttonSalvar.TabIndex = 26;
+            this.buttonSalvar.Text = "SALVAR";
+            this.buttonSalvar.UseVisualStyleBackColor = false;
+            // 
+            // buttonCancelar
+            // 
+            this.buttonCancelar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(153)))), ((int)(((byte)(93)))));
+            this.buttonCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonCancelar.Font = new System.Drawing.Font("Constantia", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonCancelar.ForeColor = System.Drawing.Color.Black;
+            this.buttonCancelar.Image = ((System.Drawing.Image)(resources.GetObject("buttonCancelar.Image")));
+            this.buttonCancelar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonCancelar.Location = new System.Drawing.Point(261, 227);
+            this.buttonCancelar.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonCancelar.Name = "buttonCancelar";
+            this.buttonCancelar.Padding = new System.Windows.Forms.Padding(16, 0, 0, 0);
+            this.buttonCancelar.Size = new System.Drawing.Size(172, 46);
+            this.buttonCancelar.TabIndex = 28;
+            this.buttonCancelar.Text = "CANCELAR";
+            this.buttonCancelar.UseVisualStyleBackColor = false;
+            // 
+            // labelTitulo
+            // 
+            this.labelTitulo.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.labelTitulo.AutoSize = true;
+            this.labelTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(242)))), ((int)(((byte)(245)))));
+            this.labelTitulo.Font = new System.Drawing.Font("Constantia", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelTitulo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(153)))), ((int)(((byte)(93)))));
+            this.labelTitulo.Location = new System.Drawing.Point(114, 23);
+            this.labelTitulo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Size = new System.Drawing.Size(233, 27);
+            this.labelTitulo.TabIndex = 29;
+            this.labelTitulo.Text = "CADASTRO/EDIÇÃO:";
+            // 
+            // ExameCadastroEdicaoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 284);
+            this.Controls.Add(this.labelTitulo);
+            this.Controls.Add(this.buttonCancelar);
+            this.Controls.Add(this.comboBoxMedico);
+            this.Controls.Add(this.labelMedico);
+            this.Controls.Add(this.buttonSalvar);
+            this.Controls.Add(this.textBoxInstrucoes);
+            this.Controls.Add(this.labelInstrucoes);
+            this.Controls.Add(this.textBoxPreco);
+            this.Controls.Add(this.labelPreco);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.labelNome);
+            this.Name = "ExameCadastroEdicaoForm";
+            this.Text = "ExameCadastroEdicaoForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Label labelNome;
+        private TextBox textBoxNome;
+        private Label labelPreco;
+        private TextBox textBoxPreco;
+        private Label labelInstrucoes;
+        private TextBox textBoxInstrucoes;
+        private Label labelMedico;
+        private ComboBox comboBoxMedico;
+        private Button buttonSalvar;
+        private Button buttonCancelar;
+        private Label labelTitulo;
+    }
+}
---
+++ Entra21-trabalho-03.SistemaDeGerenciamentoLaboratorial/Views/Exames/ExameCadastroEdicaoForm.cs
@@ -0,0 +1,12 @@
+﻿namespace Entra21_trabalho_03.SistemaDeGerenciamentoLaboratorial.Views.Exames
+{
+    public partial class ExameCadastroEdicaoForm : Form
+    {
+
+
+        public ExameCadastroEdicaoForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
